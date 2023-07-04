59dea3ccabfdad84fe6c493f8fb110fea0d3d6cb
Abram <58718191+4b3c@users.noreply.github.com>
2023-07-04 01:41:55 -0700

    bitsss

---
+++ bitsstuff.py
@@ -0,0 +1,11 @@
+
+num1 = 0b00011010 # 26 in binary
+
+print(num1, num1 >> 1, num1 << 3) # Output: 26 13 208
+
+
+print(0b00100110 | 0b00000101) # 'OR Gate' Output: 39
+print(0b00100110 & 0b00000101) # 'AND Gate' Output: 4
+print(0b00100110 ^ 0b00000101) # 'XOR Gate' Output: 35
+print(~0b00100110)             # 'NOT Gate' Output: -39
+
