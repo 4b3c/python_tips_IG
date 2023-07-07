3552b444144862b86a07f5b472bc282a59196891
Abram <58718191+4b3c@users.noreply.github.com>
2023-07-06 18:39:34 -0700

    walrus

---
+++ walrustip.py
@@ -0,0 +1,16 @@
+
+num = 14
+list1 = [12, 13, 15]
+
+if (num in list1):
+	print(num, "is in " + str(list1) + "!!")
+
+
+
+if ((num := 14) in (list1 := [12, 13, 15])):
+	print(num, "is in " + str(list1) + "!!")
+
+
+
+
+
